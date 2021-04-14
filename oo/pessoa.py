b4166ece704e67e2f3bffca5fbc9026db325dc60
leandro de menezes martins <leandro.m.martins.info@gmail.com>
2021-04-14 20:13:50 -0300

    criados atributos de instancias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self,nome = None, idade = 41):
+        self.idade = idade
+        self.nome = nome
+        
     def cumprimentar(self):
         return f'ola {id(self)}'
 
@@ -8,3 +12,11 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome='Leandro'
+    print(p.nome)
+    print(p.idade)
+
+
+
+
